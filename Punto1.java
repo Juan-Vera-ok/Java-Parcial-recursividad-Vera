372e269791cd883fb592136c7952bd6dfbc34aad
El tipazo <juanvera2001@hotmail.com>
2020-12-11 00:42:50 -0300

    Subiendo archivos

---
+++ Punto1.java
@@ -0,0 +1,21 @@
+public class Punto1
+{
+   public static void main(String[] args)
+   {
+   int n,b;
+   n=Console.readInt("Ingrese n");
+   b=Console.readInt("Ingrese b");
+   System.out.println(explosivo(n,b));
+   } 
+    
+   public static int explosivo(int n, int b)
+   {
+      if (b>=n)
+      return n;
+      else
+         
+         f(n>=b)
+         return explosivo (n/b,(n-(n/b)));
+
+   }
+}
